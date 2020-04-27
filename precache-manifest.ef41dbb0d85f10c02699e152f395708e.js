9c038dd8babd429929b6ebe03ec071eac267990a
EvilSpark <kamrantahir25@gmail.com>
2020-04-27 22:11:24 +0530

    Updates

---
+++ precache-manifest.ef41dbb0d85f10c02699e152f395708e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6b43d9927090ec83a7a9d3d33934823",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b0682c0c98f322bf3a9",
+    "url": "/static/css/main.c3ddbcaa.chunk.css"
+  },
+  {
+    "revision": "8a29faa120b8ee4f8e60",
+    "url": "/static/js/2.7f541e44.chunk.js"
+  },
+  {
+    "revision": "640aa39c32c9bb605adf655ed1141834",
+    "url": "/static/js/2.7f541e44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b0682c0c98f322bf3a9",
+    "url": "/static/js/main.0701c801.chunk.js"
+  },
+  {
+    "revision": "1301a3a06bd76ff817b9",
+    "url": "/static/js/runtime-main.84d9eb0f.js"
+  }
+]);
